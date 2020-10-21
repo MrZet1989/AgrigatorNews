867620c8bb977056a5f3f8d44f2a0f5288d6b960
GregSherm4n <gregsherm4n@protonmail.com>
2020-10-21 21:26:01 +0300

    Start of the project

---
+++ src/main/java/com/webserver/chooseyournews/ChooseyournewsApplication.java
@@ -0,0 +1,13 @@
+package com.webserver.chooseyournews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseyournewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseyournewsApplication.class, args);
+	}
+
+}
